6f195d5012a61fe9383b21a8503ab2be196554f3
Adam Johnson <adam7819@googlemail.com>
2023-08-07 21:15:08 +0100

    Add click event for seats

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied'); //grabs all seats into a node list / similar to an array
+const seats = document.querySelectorAll('.row .seat:not(.occupied'); //grabs all seats into a node list / similar to an array
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
 
-const ticketPrice = +movieSelect.nodeValue; 
+const ticketPrice = +movieSelect.value; 
 
-console.log(ticketPrice);
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
 
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+// click on seats to toggle
+container.addEventListener('click', (event) => {
+    if(event.target.classList.contains('seat') && !event.target.classList.contains('occuiped')) {
+        event.target.classList.toggle('selected');
+
+        updateSelectedCount();
+    }
+})
