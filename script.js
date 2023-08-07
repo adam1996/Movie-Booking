77a00ce5f921dd8230d4d56293d135b244b791e9
Adam Johnson <adam7819@googlemail.com>
2023-08-07 21:02:32 +0100

    Spelling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie')
 
-const ticketPrice = movieSelect.nodeValue; 
+const ticketPrice = +movieSelect.nodeValue; 
 
 console.log(ticketPrice);
 
